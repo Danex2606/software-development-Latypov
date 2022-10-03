f094b5f5ee5b20f85ee3563bc1d9ed4016fb1fde
Danex2606 <63865832+Danex2606@users.noreply.github.com>
2022-10-03 21:32:57 +0300

    Add files via upload

---
+++ практика 2/latypov_2.py
@@ -0,0 +1,92 @@
+import hashlib, time
+
+import multiprocessing as mp
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+
+def gen_passw(idx):
+    password = ""
+    for i in range(5):
+        password += alphabet[idx % len(alphabet)]
+        idx = idx // len(alphabet)
+
+    return password[::-1]
+
+
+def crack_sha256_thread(idx, hashes, start, end):
+    for i in range(start, end):
+        password = gen_passw(i)
+        hash = hashlib.sha256(password.encode('utf-8')).hexdigest()
+        for h in hashes:
+            if hash != h:
+                continue
+            print("Хэш {} найден: {}. Поток {}.".format(hash, password, idx))
+
+
+def crack_sha256():
+    start_time = time.perf_counter()
+
+    hashes = ["1115dd800feaacefdf481f1f9070374a2a81e27880f187396db67958b207cbad",
+              "3a7bd3e2360a3d29eea436fcfb7e44c735d117c42d1c1835420b6b9942dd4f1b",
+              "74e1bb62f8dabb8125a58852b63bdf6eaef667cb56ac7f7cdba6d7305c50a22f"]
+
+    N = 26**5-1
+    n_threads = int(input("Количество потоков для SHA256:"))
+    per_thread = N // n_threads
+
+    threads = []
+    start = 0
+    for i in range(n_threads):
+        end = start + per_thread
+        t = mp.Process(target=crack_sha256_thread, args=(i, hashes, start, end))
+        threads.append(t)
+        t.start()
+        start = end
+
+    for thread in threads:
+        thread.join()
+
+    end_time = time.perf_counter()
+    elapsed = end_time - start_time
+    print("Время выполнения: {}.".format(elapsed))
+
+def crack_md5_thread(idx, hashes, start, end):
+    for i in range(start, end):
+        password = gen_passw(i)
+        hash = hashlib.md5(password.encode('utf-8')).hexdigest()
+        for h in hashes:
+            if hash != h:
+                continue
+            print("Хэш {} найден: {} потоком {}.".format(hash, password, idx))
+
+def crack_md5():
+    start_time = time.perf_counter()
+
+    hashes = ["8b61c11eb8baedd53d2e99d1a01fa7bb",
+              "286a03842af6933d393d492880934fb5",
+              "2fd2f6d846b1664a8b79b2f4ead806eb"]
+
+    N = 26**5-1
+    n_threads = int(input("Количество потоков для MD5:"))
+    per_thread = N // n_threads
+
+    threads = []
+    start = 0
+    for i in range(n_threads):
+        end = start + per_thread
+        t = mp.Process(target=crack_md5_thread, args=(i, hashes, start, end))
+        threads.append(t)
+        t.start()
+        start = end
+
+    for thread in threads:
+        thread.join()
+
+    end_time = time.perf_counter()
+    elapsed = end_time - start_time
+    print("Время выполнения: {}.".format(elapsed))
+
+if __name__ == '__main__':
+    crack_sha256()
+    crack_md5()
